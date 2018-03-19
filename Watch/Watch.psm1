5702b107739df661f36fe29c47ce39cd750af9fc
markwragg <mark.wragg@gmail.com>
2018-03-19 11:41:31 +0000

    Removed \Private as not in use

--- Watch/Watch.psm1
+++ Watch/Watch.psm1
@@ -1,25 +1,20 @@
-﻿$Public  = @( Get-ChildItem -Path "$PSScriptRoot\Public\*.ps1" -Recurse )
-$Private = @( Get-ChildItem -Path "$PSScriptRoot\Private\*.ps1" -Recurse )
+﻿$Public = @( Get-ChildItem -Path "$PSScriptRoot\Public\*.ps1" -Recurse )
 
-@($Public + $Private) | ForEach-Object {
-    try
-    {
+$Public | ForEach-Object {
+    try {
         . $_.FullName
     }
-    catch
-    {
+    catch {
         Write-Error -Message "Failed to import function $($_.FullName): $_"
     }
 }
 
-if (-not (Test-Path alias:Watch)) 
-{
+if (-not (Test-Path alias:Watch)) {
     New-Alias -Name 'Watch' -Value 'Watch-Command'
     Export-ModuleMember -Function $Public.BaseName -Alias 'Watch'
 }
 
-if (-not (Test-Path alias:wc)) 
-{
+if (-not (Test-Path alias:wc)) {
     New-Alias -Name 'wc' -Value 'Watch-Command'
     Export-ModuleMember -Function $Public.BaseName -Alias 'wc'
 }
