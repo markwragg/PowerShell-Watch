8a6251fdf6b4c6d077f0332aeb4d7fc357506897
Mark Wragg <mark.wragg@gmail.com>
2018-09-17 13:10:05 +0100

    Added -SkipPublisherCheck to build.ps1

--- build.ps1
+++ build.ps1
@@ -26,7 +26,7 @@
                     
                     Write-Verbose -Message "$($ModuleName) Installed Version [$($Version.tostring())] is outdated. Installing Gallery Version [$($GalleryVersion.tostring())]"
                     
-                    Install-Module -Name $ModuleName -Force -Scope CurrentUser
+                    Install-Module -Name $ModuleName -Force -Scope CurrentUser -SkipPublisherCheck
                     Import-Module -Name $ModuleName -Force -RequiredVersion $GalleryVersion
                 }
                 else
@@ -38,7 +38,7 @@
             else
             {
                 Write-Verbose -Message "$($ModuleName) Missing, installing Module"
-                Install-Module -Name $ModuleName -Force -Scope CurrentUser
+                Install-Module -Name $ModuleName -Force -Scope CurrentUser -SkipPublisherCheck
                 Import-Module -Name $ModuleName -Force -RequiredVersion $Version
             }
         }
