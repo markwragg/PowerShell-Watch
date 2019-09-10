a242c53d14b59c3a4f95e435280c2f40f24ed992
Mark Wragg <mark.wragg@elysianit.com>
2019-09-10 12:18:20 +0100

    Removed InModuleScope

--- Tests/Watch-Command.Tests.ps1
+++ Tests/Watch-Command.Tests.ps1
@@ -8,65 +8,62 @@ Get-Module $Module | Remove-Module -Force
 Import-Module "$Root\$Module" -Force
 
 Describe "Watch-Command PS$PSVersion" {
-    
-    InModuleScope Watch {
+       
+    $InvokeCommand = Get-Command Invoke-Command
+
+    Mock Invoke-Command { & $InvokeCommand -ScriptBlock $ScriptBlock -ArgumentList $ArgumentList } -Verifiable
         
-        $InvokeCommand = Get-Command Invoke-Command
-
-        Mock Invoke-Command { & $InvokeCommand -ScriptBlock $ScriptBlock -ArgumentList $ArgumentList } -Verifiable
-        
-        Context 'Invoked via pipeline' {
+    Context 'Invoked via pipeline' {
             
-            Get-Date | Select-Object Hour, Minute, Second | Watch-Command
+        Get-Date | Select-Object Hour, Minute, Second | Watch-Command
 
-            It 'Should execute all verifiable mocks' {
-                Assert-VerifiableMock
-            }
-            It 'Should call Invoke-Command at least 2 times' {
-                Assert-MockCalled Invoke-Command -Times 2
-            }
+        It 'Should execute all verifiable mocks' {
+            Assert-VerifiableMock
         }
+        It 'Should call Invoke-Command at least 2 times' {
+            Assert-MockCalled Invoke-Command -Times 2
+        }
+    }
 
-        Context 'Invoked via scriptblock' {
+    Context 'Invoked via scriptblock' {
             
-            Watch-Command -ScriptBlock { Get-Date } -Difference -AsString
+        Watch-Command -ScriptBlock { Get-Date } -Difference -AsString
 
-            It 'Should execute all verifiable mocks' {
-                Assert-VerifiableMock
-            }
-            It 'Should call Invoke-Command at least 2 times' {
-                Assert-MockCalled Invoke-Command -Times 2
-            }
+        It 'Should execute all verifiable mocks' {
+            Assert-VerifiableMock
         }
+        It 'Should call Invoke-Command at least 2 times' {
+            Assert-MockCalled Invoke-Command -Times 2
+        }
+    }
 
-        Context 'Invoked via wc Alias' {
+    Context 'Invoked via wc Alias' {
             
-            Get-Date | wc
+        Get-Date | wc
 
-            It 'Should execute all verifiable mocks' {
-                Assert-VerifiableMock
-            }
-            It 'Should call Invoke-Command at least 2 times' {
-                Assert-MockCalled Invoke-Command -Times 2
-            }
+        It 'Should execute all verifiable mocks' {
+            Assert-VerifiableMock
         }
+        It 'Should call Invoke-Command at least 2 times' {
+            Assert-MockCalled Invoke-Command -Times 2
+        }
+    }
 
-        Context 'Invoked via watch Alias' {
+    Context 'Invoked via watch Alias' {
             
-            Get-Date | watch
+        Get-Date | watch
 
-            It 'Should execute all verifiable mocks' {
-                Assert-VerifiableMock
-            }
-            It 'Should call Invoke-Command at least 2 times' {
-                Assert-MockCalled Invoke-Command -Times 2
-            }
+        It 'Should execute all verifiable mocks' {
+            Assert-VerifiableMock
         }
+        It 'Should call Invoke-Command at least 2 times' {
+            Assert-MockCalled Invoke-Command -Times 2
+        }
+    }
 
-        Context 'Expected exceptions' {
-            It 'Should throw if a ScriptBlock is not provided unless via Pipeline' {
-                { Watch-Command -ScriptBlock Get-Date } | Should -Throw 'The -ScriptBlock parameter must be provided an object of type ScriptBlock unless invoked via the Pipeline.'
-            }
+    Context 'Expected exceptions' {
+        It 'Should throw if a ScriptBlock is not provided unless via Pipeline' {
+            { Watch-Command -ScriptBlock Get-Date } | Should -Throw 'The -ScriptBlock parameter must be provided an object of type ScriptBlock unless invoked via the Pipeline.'
         }
     }
 }
